ddd61b5d39b825445decc658a74d3c1209e7b9d4
SarthakAdhikari <adhikari.sarthak@gmail.com>
2020-12-09 16:53:43 +0545

    Remove unused AdView

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 from rest_framework import routers
-from api.v1.views.ads import AdViewSet, AdView
+from api.v1.views.ads import AdViewSet
 
 router = routers.SimpleRouter()
 
 router.register('ads', AdViewSet, basename='ads')
 
 urlpatterns = router.urls
-
-urlpatterns += [
-    path('create-ad', AdView.as_view(), name='create_ad'),
-]
