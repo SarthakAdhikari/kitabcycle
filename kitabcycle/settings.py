a8085b53fa9a5da609fa803dd4b4e4423b3b725c
SarthakAdhikari <adhikari.sarthak@gmail.com>
2020-12-10 17:08:47 +0545

    Remove STATICFILES_DIRS

--- kitabcycle/settings.py
+++ kitabcycle/settings.py
@@ -167,7 +167,6 @@ LOGGING = {
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')# location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'kitabcycle/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
