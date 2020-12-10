c80b0637029ef048abeaa11c953f883f121ccdcd
SarthakAdhikari <adhikari.sarthak@gmail.com>
2020-12-10 19:11:13 +0545

    Change router to DefaultRouter

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework import routers
 from api.v1.views.ads import AdViewSet
 
-router = routers.SimpleRouter()
+router = routers.DefaultRouter()
 
 router.register('ads', AdViewSet, basename='ads')
 
